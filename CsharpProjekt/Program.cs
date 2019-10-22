366cc4cf6da458d8fb95f603fc5bbe674748f7f0
BaggenBengt <47028924+BaggenBengt@users.noreply.github.com>
2019-10-22 16:50:11 +0200

    Update Program.cs

--- CsharpProjekt/Program.cs
+++ CsharpProjekt/Program.cs
@@ -19,6 +19,7 @@ namespace CsharpProjekt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            string i = "Hej";
         }
     }
 }
